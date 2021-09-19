a1083f639552edf6e6801283b27a3db54cde159a
Eric <emjyoon@gmail.com>
2021-09-18 23:06:23 -0700

    Slash commands working

---
+++ DiscordMusicPlayer/README.md
@@ -0,0 +1,6 @@
+# DiscordMusicPlayer
+SUSAN >:(
+
+## Configuration Options
+- `discord.token`: your Discord bot token.
+- `discord.commands_debug_guild`: set to a guild ID to register guild commands intead of global commands for easier debugging.
--- DiscordMusicPlayer/src/main/java/DiscordClient.java
+++ DiscordMusicPlayer/src/main/java/DiscordClient.java
@@ -1,8 +1,12 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 public class DiscordClient extends ListenerAdapter {
     JDA client;
@@ -19,7 +23,20 @@ public class DiscordClient extends ListenerAdapter {
     }
 
     void registerSlashCommands(){
-        client.upsertCommand("play", "Play a track.").queue();
+        String debugGuildId = Main.config.getString("discord.commands_debug_guild");
+        CommandListUpdateAction updateAction;
+        if(debugGuildId != null){
+            Guild debugGuild = client.getGuildById(debugGuildId);
+            updateAction = debugGuild.updateCommands();
+        } else {
+            updateAction = client.updateCommands();
+        }
+        updateAction.addCommands(new CommandData("play", "Add a track to the end of the queue.").addOption(OptionType.STRING, "track", "The track to play", true));
+        updateAction.addCommands(new CommandData("playtop", "Add a track to the beginning of the queue.").addOption(OptionType.STRING, "track", "The track to play", true));
+        updateAction.addCommands(new CommandData("skip", "Skips the next track(s) in the queue.").addOption(OptionType.INTEGER, "count", "How many tracks to skip.", false));
+        updateAction.addCommands(new CommandData("pause", "Pause playback."));
+        updateAction.addCommands(new CommandData("resume", "Resume playback."));
+        updateAction.queue();
     }
 
     @Override
@@ -30,6 +47,19 @@ public class DiscordClient extends ListenerAdapter {
 
     @Override
     public void onSlashCommand(SlashCommandEvent event){
-
+        if(event.getName().equals("play")){
+            event.deferReply().queue();
+            // TODO: after operation, event.getHook().sendMessage("blah").queue();
+        } else if(event.getName().equals("playtop")){
+            // TODO
+        } else if(event.getName().equals("skip")){
+            // TODO
+        } else if(event.getName().equals("pause")){
+            // TODO
+        } else if(event.getName().equals("resume")){
+            // TODO
+        } else {
+            System.out.println("Unrecognized command: " + event.getName());
+        }
     }
 }
